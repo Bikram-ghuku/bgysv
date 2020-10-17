b4efe730cbcef53bfddfead51284073d2c89c864
Bikram ghuku <bikramghuku05@gmail.com>
2020-10-17 18:41:18 +0530

    LOL

---
+++ files/Moderation.py
@@ -0,0 +1,129 @@
+from discord.ext import commands
+import discord
+import requests
+import sqlite3
+from discord.utils import get
+conn = sqlite3.connect('./db/data.sqlite')
+cursor = conn.cursor()
+
+class Moderation(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True,help="Kicks a member with the provided reason.")
+    @commands.has_guild_permissions(kick_members=True)
+    async def kick(self,ctx, member : discord.Member,*,reason:str=None):
+        message = ctx.message
+        server = ctx.message.guild
+        await message.delete()
+        try:
+            print(member)
+            kick_message_target = discord.Embed(title="Kick message",description=f"you have been kicked in {str(server)} as {reason}",color=discord.Color.blue())
+            kick_message = discord.Embed(title="Successfuly kicked" ,description=f"The following user has been kicked: {member} ✅",color=discord.Color.blue())
+            kick_message_target.set_footer(text="Developed By Bikram Ghuku")
+            kick_message.set_footer(text="Developed By Bikram Ghuku")
+            await member.send(embed=kick_message_target)
+            await ctx.message.channel.send(embed=kick_message)
+            await member.kick(reason=reason)
+        except Exception:
+            await member.kick(reason=reason)
+    
+
+    @commands.command(pass_context=True,help="Bans a member with a given reason")
+    @commands.has_guild_permissions(ban_members=True)
+    async def ban(self,ctx, member : discord.Member,*,reason:str=None):
+        message = ctx.message
+        server = ctx.message.guild
+        await message.delete()
+        if message.author.server_permissions.ban_members:
+            try:
+                print(member)
+                kick_message_target = discord.Embed(title="Ban message",description=f"you have been banned in {str(server)} as {reason}",color=discord.Color.blue())
+                kick_message = discord.Embed(title="Successfuly banned" ,description=f"The following user has been Banned: {member} ✅",color=discord.Color.blue())
+                kick_message_target.set_footer(text="Developed By Bikram Ghuku")
+                kick_message.set_footer(text="Developed By Bikram Ghuku")
+                await member.send(embed=kick_message_target)
+                await ctx.message.channel.send(embed=kick_message)
+                await member.ban(reason=reason)
+            except Exception:
+                await member.ban(reason=reason)
+
+    @commands.command(pass_context=True,help="Mutes a member and stops him from typing anything else")
+    @commands.has_guild_permissions(manage_roles=True)
+    async def mute(self,ctx, member:discord.Member,*,reason:str=None):
+        await ctx.message.delete()
+        server = ctx.message.guild
+        role = discord.utils.get(server.roles,name="Muted")
+        embene = discord.Embed(title="Muted",description=f"You have in muted in {str(server)} for {str(reason)} you won't be automatically unmuted. You need to contact a moderator for unmute", color=discord.Color.blue())
+        embene2 = discord.Embed(title="Success",description=f"✅ Successfully muted {str(member)}",color=discord.Color.blue())
+        embene2.set_footer(text="Developed By Bikram Ghuku")
+        embene.set_footer(text="Developed By Bikram Ghuku")
+        await member.send(embed=embene)
+        await ctx.message.channel.send(embed=embene2)
+        await member.add_roles(role)
+
+    @commands.command(pass_context=True,help="Unmutes a member who is muted")
+    @commands.has_guild_permissions(manage_roles=True)
+    async def unmute(self,ctx,member :discord.Member):
+        await ctx.message.delete()
+        for role_mention in member.roles:
+            if role_mention.name=="Muted":
+                server = ctx.message.guild
+                role = discord.utils.get(server.roles,name="Muted")
+                embene = discord.Embed(title="Unmute",description=f"You have in unmuted in {str(server)} ", color=discord.Color.blue())
+                embene2 = discord.Embed(title="Success",description=f"✅ Successfully unmuted {str(member)}",color=discord.Color.blue())
+                embene.set_footer(text="Developed By Bikram Ghuku")
+                embene2.set_footer(text="Developed By Bikram Ghuku")
+                await ctx.message.channel.send(embed=embene2)
+                await member.send(embed=embene)
+                await member.remove_roles(role)
+            else:
+                pass
+    
+    @commands.command(pass_context=True,help="Warns a member with a reason(requires manage server permission)")
+    @commands.has_permissions(manage_guild=True)
+    async def warn(self,ctx,member:discord.Member,*,reason:str=None):
+        await ctx.message.delete()
+        try:
+            await member.send(f"You were warned in {ctx.guild} for \n `{reason}`")
+            embene2 = discord.Embed(title="Success",description=f"✅ Successfully warned {str(member)}",color=discord.Color.blue())
+            embene2.set_footer(text="Developed By Bikram Ghuku")
+            await ctx.send(embed=embene2)
+        except Exception as e:
+            embene2 = discord.Embed(title="Error",description=f"❌ Couldn't warn {str(member)} error: {e}",color=discord.Color.red())
+            embene2.set_footer(text="Developed By Bikram Ghuku")
+            await ctx.send(embed=embene2)
+
+
+
+    @commands.command(pass_context=True,help="Strike a member with a reason(Requires admin permissions)")
+    @commands.has_permissions(administrator=True)
+    async def strike(self,ctx,member:discord.Member,*,reason:str=None):
+        await ctx.message.delete()
+        try:
+            cursor.execute(f"SELECT count FROM strike_logs WHERE server_id=\"{ctx.guild.id}\" AND user_id=\"{member.id}\"")
+            x = cursor.fetchone()
+            await ctx.send(x)
+            if x is None:
+                cursor.execute(f"INSERT INTO strike_logs(count,user_id,server_id) VALUES(\"1\",\"{member.id}\",\"{ctx.guild.id}\")")
+                conn.commit()
+            else:
+                strikes = int(x[0])+1
+                cursor.execute(f"UPDATE strike_logs SET count=\"{strikes}\" WHERE user_id=\"{member.id}\" AND server_id=\"{ctx.guild.id}\"")
+                conn.commit()
+            await member.send(f"You were striked in {ctx.guild} for \n `{reason}` you currently have `{int(x[0])+1}`")
+            embene2 = discord.Embed(title="Success",description=f"✅ Successfully striked {str(member)}",color=discord.Color.blue())
+            embene2.set_footer(text="Developed By Bikram Ghuku")
+            await ctx.send(embed=embene2)
+        except Exception as e:
+            embene2 = discord.Embed(title="Error",description=f"❌ Couldn't strike {str(member)} error: {e}",color=discord.Color.red())
+            embene2.set_footer(text="Developed By Bikram Ghuku")
+            await ctx.send(embed=embene2)
+
+
+
+
+
+
+def setup(bot):
+    bot.add_cog(Moderation(bot))
---
+++ files/YoutubeStats.py
@@ -0,0 +1,120 @@
+from discord.ext import commands
+import discord
+import requests
+import sqlite3
+from discord.utils import get
+
+
+conn = sqlite3.connect('./db/data.sqlite')
+cursor = conn.cursor()
+
+key = "AIzaSyA2Jr4pUoJ7Ry3F9RE2H76knGtF7OQDZRA"
+
+
+def get_data(channelid):
+    url = "https://www.googleapis.com/youtube/v3/channels?part=statistics&key="+key+"&id="+channelid
+    datax = requests.get(url)
+    datay = datax.json()
+    subs = datay["items"][0]["statistics"]
+    return subs
+
+
+
+def store_data(guild_id,yt_id,subs_id,views_id,vides_id):
+    cursor.execute(f"SELECT * FROM ytdata WHERE server_id ={guild_id}")
+    x = cursor.fetchone()
+    if x is None:
+        try:
+            cursor.execute(f"INSERT INTO ytdata(server_id,channel_id,views_id,videoes_id,subs_id) VALUES(\"{guild_id}\",\"{yt_id}\",\"{views_id}\",\"{vides_id}\",\"{subs_id}\")")
+            conn.commit()
+        except Exception as e:
+            print(e)
+    else:
+        cursor.execute(f"UPDATE ytdata SET channel_id=\"{yt_id}\",views_id=\"{views_id}\",subs_id=\"{subs_id}\",videoes_id=\"{vides_id}\"")
+        conn.commit()
+        
+
+
+def check_id(yt_id):
+    url = "https://www.googleapis.com/youtube/v3/channels?part=statistics&key="+key+"&id="+yt_id
+    datax = requests.get(url)
+    datay = datax.json()
+    subs = datay["pageInfo"]["resultsPerPage"]
+    return subs
+
+
+
+
+class YoutubeStats(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True,help="Create a yt stats of the channel with the help of channel url")
+    async def create(self,ctx,url):
+        guild = ctx.message.guild
+        server = str(ctx.message.guild.id)
+        urlus = url.split('/')
+        try:
+            if(len(urlus[4])==24):
+                if(check_id(str(urlus[4]))>=1):
+                    await ctx.message.channel.send("Creating YouTube stats")
+                    x = get_data(urlus[4])
+                    subs = await guild.create_voice_channel("Subscribers : "+x['subscriberCount'])
+                    videoes = await guild.create_voice_channel("Video : "+x['videoCount'])
+                    views = await guild.create_voice_channel("Views : "+x['viewCount'])
+                    store_data(str(server),str(urlus[4]),str(subs.id),str(views.id),str(videoes.id))
+                else:
+                    await ctx.message.channel.send("No channel with that Id")
+            else:
+                await ctx.message.channel.send("The link is not the correct link")
+        except Exception as e:
+            print(e)
+            await ctx.message.channel.send("an unknown error occured please contact us by joining our discord server")
+
+
+
+    @commands.command(pass_context=True,help=None)
+    async def update(self,ctx):
+        if ctx.author.id == 531138466990260225:
+            succ_msg = discord.Embed(title="Server Update ",description="The following is the result of data")
+            cursor.execute("SELECT * FROM ytdata")
+            var = cursor.fetchall()
+            for key in var:
+                cursor.execute(f"SELECT subs_id FROM ytdata WHERE server_id={str(key[0])}")
+                subs_id = cursor.fetchone()
+                subs_id_int = subs_id[0]
+                cursor.execute(f"select views_id from ytdata where server_id={key[0]}")
+                views_id = cursor.fetchone()
+                views_id_int = views_id[0]
+                cursor.execute(f"select videoes_id from ytdata where server_id={key[0]}")
+                vids_id = cursor.fetchone()
+                vids_id_int = vids_id[0]
+                cursor.execute(f"SELECT channel_id from ytdata where server_id={key[0]}")
+                yt_id = cursor.fetchone()
+                yt_id_int = yt_id[0]
+                zaaa = get_data(str(yt_id_int))
+                subscriber_present = zaaa["subscriberCount"]
+                views_present = zaaa["viewCount"]
+                video_present = zaaa["videoCount"]
+                try:
+                    subs_channel = self.bot.get_channel(int(subs_id_int))
+                    vids_channel = self.bot.get_channel(int(vids_id_int))
+                    views_channel = self.bot.get_channel(int(views_id_int))
+                    await subs_channel.edit(name="Subscriber : "+subscriber_present)
+                    await vids_channel.edit(name="videos : "+video_present)
+                    await views_channel.edit(name="Views : "+views_present)
+                    print("Update command received")
+                    succ_msg.add_field(name="Server Updated", value=f"Server updated {key} ✅")
+                except Exception as e:
+                    succ_msg.add_field(name="Error Updating", value=f"Error updating the server {key} ❌. The following error has occured : {e} ",inline=False)
+                    succ_msg.set_footer(text="Developed By Bikram Ghuku")
+
+                await ctx.message.channel.send(embed=succ_msg)
+        else:
+            war_msg = discord.Embed(title="Warning message",description="You cannot use the command ❌",color=discord.Color.red())
+            war_msg.set_footer(text="Developed By Bikram Ghuku")
+            await ctx.message.channel.send(embed=war_msg)
+
+
+def setup(bot):
+    bot.add_cog(YoutubeStats(bot))
